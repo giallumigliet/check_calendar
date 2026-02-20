5f0412961c5c37762200cff438e97acaaa4c8d93
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-02-20 16:49:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 import { createCalendar } from "./calendar.js";
 import { listenClickCalendar } from "./calendar.js";
 import { listenMonthCalendar } from "./calendar.js";
-import { updateProgress } from "./calendar.js";
+import { listenTaskButtons } from "./calendar.js";
 
 
+const calendarWrapper = document.getElementById("calendar-wrapper");
 const monthYear = document.getElementById("monthYear");
 const calendarDays = document.getElementById("calendarDays");
 
 const dayActions = document.getElementById("day-actions");
 const addBtn = document.getElementById("add-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+const taskBtn = document.getElementById("task-btn");
+const goBackButton = document.getElementById("goback-btn");
+
+const taskWrapper = document.getElementById("task-wrapper");
 
 const progressBar = document.getElementById("progressBar");
 const progressText = document.getElementById("progressText");
@@ -28,5 +33,6 @@ createCalendar(date, monthYear, calendarDays);
 
 listenClickCalendar(addBtn, cancelBtn, dayActions, calendarDays, progressBar, progressText);
 
-listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn)
+listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn);
 
+listenTaskButtons(taskBtn, goBackButton, calendarWrapper, taskWrapper);
