5982e554cae624c4c573f8dbf1cad84321574477
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-02-25 11:19:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ import { createCalendar } from "./calendar.js";
 import { listenClickCalendar } from "./calendar.js";
 import { listenMonthCalendar } from "./calendar.js";
 import { listenTaskButtons } from "./calendar.js";
+import { listenHue } from "./calendar.js";
+import { listenPanelButtons } from "./calendar.js";
+import { listenSaveTask } from "./calendar.js";
 
 
 const calendarWrapper = document.getElementById("calendar-wrapper");
@@ -13,26 +16,16 @@ const addBtn = document.getElementById("add-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const taskBtn = document.getElementById("task-btn");
 
-
 const progressBar = document.getElementById("progressBar");
 const progressText = document.getElementById("progressText");
 
 const prevMonthBtn = document.getElementById("prevMonth");
 const nextMonthBtn = document.getElementById("nextMonth");
 
-
 const panel = document.getElementById("floating-panel");
 const overlay = document.getElementById("overlay");
 const closePanel = document.getElementById("close-panel");
 const buttonFooter = document.getElementById("button-footer");
-
-
-
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth();
-
-
 
 const addTaskBtn = document.getElementById("addTask-btn");
 const saveTaskBtn = document.getElementById("save-task");
@@ -49,6 +42,12 @@ const hueContainer = document.getElementById("hue-container");
 
 const modifyTaskBtn = document.getElementById("modifyTask-btn");
 
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+
+
+
 createCalendar(date, monthYear, calendarDays);
 
 listenClickCalendar(addBtn, cancelBtn, dayActions, calendarDays, progressBar, progressText);
@@ -57,96 +56,12 @@ listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn);
 
 listenTaskButtons(taskBtn, closePanel, panel, overlay, calendarWrapper, buttonFooter, taskManager, taskForm, modifyTaskBtn);
 
+listenPanelButtons(addTaskBtn, goBackBtn, modifyTaskBtn, taskManager, taskForm, hueContainer);
 
+listenHue(huePreview, hueContainer, taskHueInput);
 
+listenSaveTask(saveTaskBtn, taskList, taskNameInput, taskHueInput, huePreview, taskManager, taskForm);
 
-
-
-addTaskBtn.addEventListener("click", () => {
-  taskManager.classList.add("hidden-task-buttons");
-  taskForm.classList.remove("hidden-task-buttons");
-  hueContainer.classList.add("hidden-task-buttons");
-
-  const badges = document.querySelectorAll(".delete-badge");
-  badges.forEach(badge => {
-    badge.classList.add("hidden");
-  });
-  modifyTaskBtn.classList.remove("modify-active");
-});
-
-
-goBackBtn.addEventListener("click", () => {
-  taskManager.classList.remove("hidden-task-buttons");
-  taskForm.classList.add("hidden-task-buttons");
-});
-
-
-huePreview.addEventListener("click", () => {
-  hueContainer.classList.toggle("hidden-task-buttons");
-});
-
-taskHueInput.addEventListener("input", () => {
-  const hue = taskHueInput.value;
-  huePreview.style.backgroundColor = `hsl(${hue}, 90%, 55%)`;
-});
-
-
-
-saveTaskBtn.addEventListener("click", () => {
-  const name = taskNameInput.value.trim();
-  const hue = taskHueInput.value;
-
-  if (!name) return;
-
-  const newTask = document.createElement("div");
-  newTask.classList.add("task-item");
-  newTask.dataset.hue = hue;
-  newTask.style.backgroundColor = `hsl(${hue}, 90%, 45%)`;
-  newTask.textContent = name;
-
-  // crea badge delete
-  const deleteBadge = document.createElement("div");
-  deleteBadge.classList.add("delete-badge", "hidden");
-  deleteBadge.textContent = "━";
-
-  newTask.appendChild(deleteBadge);
-  taskList.appendChild(newTask);
-
-  deleteBadge.addEventListener("click", (e) => {
-    if (confirm("Press OK to delete the task.")) {
-        e.stopPropagation(); // evita click sulla task
-        newTask.remove();
-    }
-  });
-
-  // reset form
-  taskNameInput.value = "";
-  taskHueInput.value = 162;
-  huePreview.style.backgroundColor = `hsl(162, 90%, 55%)`;
-
-  
-  taskForm.classList.add("hidden-task-buttons");
-  taskManager.classList.remove("hidden-task-buttons");
-
-  newTask.addEventListener("click", () => {
-    document.documentElement.style.setProperty("--main-hue", newTask.dataset.hue);
-  });
-});
-
-
-
-modifyTaskBtn.addEventListener("click", () => {
-  const badges = document.querySelectorAll(".delete-badge");
-
-  badges.forEach(badge => {
-    badge.classList.toggle("hidden");
-  });
-
-  modifyTaskBtn.classList.toggle("modify-active");
-  
-  taskManager.classList.remove("hidden-task-buttons");
-  taskForm.classList.add("hidden-task-buttons");
-});
 
 
 
