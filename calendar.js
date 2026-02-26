d6d845b2ed22ec26d0bde78aa130bf14547e129d
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-02-26 11:44:15 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -250,11 +250,30 @@ export function listenHue(huePreview, hueContainer, taskHueInput) {
 
 
 export function listenSaveTask(saveTaskBtn, taskList, taskNameInput, taskHueInput, huePreview, taskManager, taskForm) {
-    saveTaskBtn.addEventListener("click", () => {
+    saveTaskBtn.addEventListener("click", () => { 
+        const user = auth.currentUser;
+        if (!user) return alert("Login required");
+    
         const name = taskNameInput.value.trim();
-        const hue = taskHueInput.value;
-
+        const hue = Number(taskHueInput.value);
         if (!name) return;
+    
+        try {
+            const taskRef = await addDoc(
+                collection(db, "users", user.uid, "tasks"),
+                {
+                    name: name,
+                    hue: hue,
+                    createdAt: serverTimestamp(),
+                    completions: {}
+                }
+            );
+    
+            console.log("Task salvata con ID:", taskRef.id);
+        } catch (error) {
+            console.error(error);
+        }
+        
 
         const newTask = document.createElement("div");
         newTask.classList.add("task-item");
@@ -291,5 +310,6 @@ export function listenSaveTask(saveTaskBtn, taskList, taskNameInput, taskHueInpu
         });
     });
 }
+
 
 
