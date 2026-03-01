2082f859d5885670cfed9f1df0398a1c23bd9cef
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-03-01 12:54:53 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,323 +1,173 @@
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { db, auth } from "./script.js";
+// Import Firebase for database
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, setPersistence, browserLocalPersistence, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Import functions 
+import { createCalendar, listenClickCalendar, listenMonthCalendar, listenTaskButtons, listenHue, listenPanelButtons, listenSaveTask, createTaskList} from "./calendar.js";
 
 
-export function createCalendar(date, monthYear, calendarDays) {
-    const year = date.getFullYear();
-    const month = date.getMonth();
+
+// Getting html elements
+const calendarWrapper = document.getElementById("calendar-wrapper");
+const monthYear = document.getElementById("monthYear");
+const calendarDays = document.getElementById("calendarDays");
+
+const dayActions = document.getElementById("day-actions");
+const addBtn = document.getElementById("add-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const taskBtn = document.getElementById("task-btn");
+
+const progressBar = document.getElementById("progressBar");
+const progressText = document.getElementById("progressText");
+
+const prevMonthBtn = document.getElementById("prevMonth");
+const nextMonthBtn = document.getElementById("nextMonth");
+
+const panel = document.getElementById("floating-panel");
+const overlay = document.getElementById("overlay");
+const closePanel = document.getElementById("close-panel");
+const buttonFooter = document.getElementById("button-footer");
+
+const addTaskBtn = document.getElementById("addTask-btn");
+const saveTaskBtn = document.getElementById("save-task");
+const goBackBtn = document.getElementById("goback-task");
+
+const taskList = document.getElementById("task-list");
+const taskManager = document.getElementById("task-manager");
+const taskForm = document.getElementById("task-form");
+
+const taskNameInput = document.getElementById("task-name");
+const taskHueInput = document.getElementById("task-hue");
+const huePreview = document.getElementById("hue-preview");
+const hueContainer = document.getElementById("hue-container");
+
+const modifyTaskBtn = document.getElementById("modifyTask-btn");
+
+
+const loginBtn = document.getElementById("login-btn");
+const logoutBtn = document.getElementById("logout-btn");
+const profileButton = document.getElementById("profile-btn");
+const userPhoto = document.getElementById("user-photo");
+const accountPanel = document.getElementById("account-floating-panel");
+const changeAccountBtn = document.getElementById("changeAccount-btn");
+
+let tasks = [];
+let currentTask = '';
+
+
+// Date
+const date = new Date();
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOkre2FhmRFlSBPYznZUVAJxLQh-QeExc",
+  authDomain: "check-calendar-giallumigliet.firebaseapp.com",
+  projectId: "check-calendar-giallumigliet",
+  storageBucket: "check-calendar-giallumigliet.firebasestorage.app",
+  messagingSenderId: "741223614800",
+  appId: "1:741223614800:web:41af2763f7c3c6ebb5c455"
+};
+// Firebase initialization
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+await setPersistence(auth, browserLocalPersistence);
+export const db = getFirestore(app);
+
+// Login Google
+const provider = new GoogleAuthProvider();
+
+
+//---------------------------------------------
+//---------------------------------------------
+loginBtn.addEventListener("click", async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    console.log("Utente:", result.user);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+changeAccountBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  
+  try {
+    const result = await signInWithPopup(auth, provider);
+    console.log("Utente:", result.user);
+  } catch (error) {
+    console.error(error);
+  }
+  
+  accountPanel.classList.add("hidden-task-buttons");
+});
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  accountPanel.classList.add("hidden-task-buttons");
+});
+
+profileButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  accountPanel.classList.toggle("hidden-task-buttons");
+});
+
+document.addEventListener("click", function(e) {
+  const clickedInsidePanel = accountPanel.contains(e.target);
+  const clickedProfileBtn = profileButton.contains(e.target);
+
+  if (!clickedInsidePanel && !clickedProfileBtn) {
+    accountPanel.classList.add("hidden-task-buttons");
+  }
+});
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // Mostra UI utente
+    loginBtn.classList.add("hidden-task-buttons");
+    profileButton.classList.remove("hidden-task-buttons");
+
+    // Imposta foto profilo
+    userPhoto.src = user.photoURL;
+
     
-    const months = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
+    const tasksRef = collection(db, "users", uid, "tasks");
+      
+    onSnapshot(tasksRef, (snapshot) => {
+      tasks = [];
 
-    monthYear.textContent = months[month] + " " + year;
-    calendarDays.innerHTML = "";
-
-
-    const firstDay = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    // First day being Monday in Italy
-    const startDay = (firstDay === 0) ? 7 : firstDay;
-
-
-    // to display days of the months
-    for (let day = 1; day <= daysInMonth; day++) {
-        const dayDiv = document.createElement("div");
-        dayDiv.textContent = day;
-        dayDiv.classList.add("day");
-
-        // to shift the first day
-        if (day === 1) {
-            dayDiv.style.gridColumnStart = startDay;
-        }
-
-        if (
-            day === date.getDate() &&
-            month === new Date().getMonth() &&
-            year === new Date().getFullYear()
-        ) {
-            dayDiv.classList.add("today");
-        }
-
-        calendarDays.appendChild(dayDiv);
-    }
-}
-
-
-
-export function listenClickCalendar(addBtn, cancelBtn, dayActions, calendarDays, progressBar, progressText) {
-    let selectedDay=null;
-    
-    // to select the day by clicking it
-    calendarDays.addEventListener("click", function(e) {
-        selectedDay = e.target;
-        if (selectedDay.tagName === "DIV") {
-            
-            // remove previous selection
-            document.querySelectorAll(".days div").forEach(day => {
-            day.classList.remove("selected");
-            });
-            
-            // select clicked day
-            selectedDay.classList.add("selected");
-
-            // show day actions
-            dayActions.classList.remove("hidden-day-buttons");
-            
-        }
-    });
-
-    
-
-    // to unselect the day by clicking outside
-    document.addEventListener("click", function(e) {
-        const clickedDay = e.target.closest(".days div");
-        const clickedDayActions = e.target.closest("#day-actions");
-
-        // if no day or add/cancel button button is clicked
-        if (!clickedDay && !clickedDayActions) {
-            
-            // remove selection
-            document.querySelectorAll(".days div").forEach(day => {
-            day.classList.remove("selected");
-            });
-
-            selectedDay = null;
-
-            // hide day actions
-            dayActions.classList.add("hidden-day-buttons");
-        }
-
-
-        
-    });
-
-
-    // day completed
-    addBtn.addEventListener("click", function() {
-
-        if (selectedDay) {
-            selectedDay.classList.remove("selected");
-            selectedDay.classList.add("completed");
-            selectedDay = null;
-
-            dayActions.classList.add("hidden-day-buttons");
-            updateProgress(calendarDays, progressBar, progressText);
-        }
-    });
-
-    // day annulled
-    cancelBtn.addEventListener("click", function() {
-
-        if (selectedDay) {
-            selectedDay.classList.remove("selected");
-            selectedDay.classList.remove("completed");
-            selectedDay = null;
-
-            dayActions.classList.add("hidden-day-buttons");
-            updateProgress(calendarDays, progressBar, progressText);
-        }
-    });
-
-
-}
-
-
-
-export function listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn) {
-    // left arrow
-    prevMonthBtn.addEventListener("click", () => {
-        date.setMonth(date.getMonth() - 1);
-        createCalendar(date, monthYear, calendarDays);
-        updateProgress(calendarDays, progressBar, progressText);
-    });
-
-    // right arrow
-    nextMonthBtn.addEventListener("click", () => {
-        date.setMonth(date.getMonth() + 1);
-        createCalendar(date, monthYear, calendarDays);
-        updateProgress(calendarDays, progressBar, progressText);
-    });
-}
-
-
-
-export function listenTaskButtons(taskBtn, closePanel, panel, overlay, calendarWrapper, buttonFooter, taskManager, taskForm, modifyTaskBtn) {
-    taskBtn.addEventListener("click", () => {
-        calendarWrapper.classList.add("hidden-day-buttons");
-        buttonFooter.classList.add("hidden-day-buttons");
-        panel.classList.add("active");
-        overlay.classList.add("active");
-
-        taskManager.classList.remove("hidden-task-buttons");
-        taskForm.classList.add("hidden-task-buttons");
-
-        const badges = document.querySelectorAll(".delete-badge");
-        badges.forEach(badge => {
-            badge.classList.add("hidden");
+      snapshot.forEach(doc => {
+        tasks.push({
+          id: doc.id,
+          ...doc.data()
         });
-
-        modifyTaskBtn.classList.remove("modify-active");
-    });
-
-    closePanel.addEventListener("click", () => {
-        calendarWrapper.classList.remove("hidden-day-buttons");
-        buttonFooter.classList.remove("hidden-day-buttons");
-        panel.classList.remove("active");
-        overlay.classList.remove("active");
-        
-        requestAnimationFrame(() => {
-          document.documentElement.style.backgroundColor = "#ffffff";
-        })
-
-    });
-
-    overlay.addEventListener("click", () => {
-        calendarWrapper.classList.remove("hidden-day-buttons");
-        buttonFooter.classList.remove("hidden-day-buttons");
-        panel.classList.remove("active");
-        overlay.classList.remove("active");
-
-        requestAnimationFrame(() => {
-          document.documentElement.style.backgroundColor = "#ffffff";
-        })
-
-    });
-}
-
-
-
-
-export function updateProgress(calendarDays, progressBar, progressText) {
-    const completedNumber = calendarDays.querySelectorAll(".day.completed").length;
-    const total = calendarDays.querySelectorAll(".day").length;
-    
-    
-    if (total === 0) return; 
-    const percent = (completedNumber / total) * 100;
-
-    progressBar.style.width = percent + "%";
-    progressText.textContent = completedNumber; 
-}
-
-
-
-export function listenPanelButtons(addTaskBtn, goBackBtn, modifyTaskBtn, taskManager, taskForm, hueContainer) {
-    addTaskBtn.addEventListener("click", () => {
-      taskManager.classList.add("hidden-task-buttons");
-      taskForm.classList.remove("hidden-task-buttons");
-      hueContainer.classList.add("hidden-task-buttons");
-    
-      const badges = document.querySelectorAll(".delete-badge");
-      badges.forEach(badge => {
-        badge.classList.add("hidden");
       });
-      modifyTaskBtn.classList.remove("modify-active");
     });
+  } else {
+    // Mostra login
+    loginBtn.classList.remove("hidden-task-buttons");
+    profileButton.classList.add("hidden-task-buttons");
+  }
+});
 
 
-    modifyTaskBtn.addEventListener("click", () => {
-        const badges = document.querySelectorAll(".delete-badge");
+//---------------------------------------------
+//---------------------------------------------
+createCalendar(date, monthYear, calendarDays);
 
-        badges.forEach(badge => {
-            badge.classList.toggle("hidden");
-        });
+listenClickCalendar(addBtn, cancelBtn, dayActions, calendarDays, progressBar, progressText);
 
-        modifyTaskBtn.classList.toggle("modify-active");
-        
-        taskManager.classList.remove("hidden-task-buttons");
-        taskForm.classList.add("hidden-task-buttons");
-    });
-    
-    
-    goBackBtn.addEventListener("click", () => {
-      taskManager.classList.remove("hidden-task-buttons");
-      taskForm.classList.add("hidden-task-buttons");
-    });
-}
+listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn);
 
+listenTaskButtons(taskBtn, closePanel, panel, overlay, calendarWrapper, buttonFooter, taskManager, taskForm, modifyTaskBtn);
 
+listenPanelButtons(addTaskBtn, goBackBtn, modifyTaskBtn, taskManager, taskForm, hueContainer);
 
-export function listenHue(huePreview, hueContainer, taskHueInput) {
-    huePreview.addEventListener("click", () => {
-      hueContainer.classList.toggle("hidden-task-buttons");
-    });
+listenHue(huePreview, hueContainer, taskHueInput);
 
-    taskHueInput.addEventListener("input", () => {
-      const hue = taskHueInput.value;
-      huePreview.style.backgroundColor = `hsl(${hue}, 90%, 55%)`;
-    });
-}
+listenSaveTask(saveTaskBtn, taskNameInput, taskHueInput, huePreview, taskManager, taskForm, tasks);
 
+createTaskList(taskList, tasks, currentTask)
 
-
-
-export function listenSaveTask(saveTaskBtn, taskNameInput, taskHueInput, huePreview, taskManager, taskForm, tasks) {
-    saveTaskBtn.addEventListener("click", () => {
-        const name = taskNameInput.value.trim();
-        const hue = taskHueInput.value;
-
-        if (!name || tasks.includes(name)) return;
-
-        // creating in DB
-        setDoc(
-            doc(db, "users", uid, "tasks", name),
-            { color: hue }
-        );
-
-        // reset form
-        taskNameInput.value = "";
-        taskHueInput.value = 162;
-        huePreview.style.backgroundColor = `hsl(162, 90%, 55%)`;
-
-        
-        taskForm.classList.add("hidden-task-buttons");
-        taskManager.classList.remove("hidden-task-buttons");
-
-        newTask.addEventListener("click", () => {
-            document.documentElement.style.setProperty("--main-hue", newTask.dataset.hue);
-        });
-    });
-}
-
-
-
-export function createTaskList(taskList, tasks, currentTask) {
-  taskList.innerHTML = ""; // opzionale: pulisce la lista prima di ricrearla
-
-  tasks.forEach(task => {
-    const { id: name, color: hue } = task; // id = taskName, color = hue
-
-    const newTask = document.createElement("div");
-    newTask.classList.add("task-item");
-    newTask.dataset.hue = hue;
-    newTask.style.backgroundColor = `hsl(${hue}, 90%, 45%)`;
-    newTask.textContent = name;
-
-    // crea badge delete
-    const deleteBadge = document.createElement("div");
-    deleteBadge.classList.add("delete-badge", "hidden");
-    deleteBadge.textContent = "━";
-
-    newTask.appendChild(deleteBadge);
-    taskList.appendChild(newTask);
-
-    deleteBadge.addEventListener("click", (e) => {
-      if (confirm("Press OK to delete the task.")) {
-        e.stopPropagation();
-        newTask.remove();
-        // deleting task from DB
-        deleteDoc(
-            doc(db, "users", uid, "tasks", name)
-        );
-      }
-    });
-
-    newTask.addEventListener("click", () => {
-      document.documentElement.style.setProperty("--main-hue", newTask.dataset.hue);
-      currentTask = newTask.textContent;
-    });
-  });
-}
 
