a848a7e0e025aa2305e12a35d1c07c6fbf568544
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-03-04 18:48:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12
 
 import {
   createCalendar, listenClickCalendar, listenMonthCalendar, listenTaskButtons,
-  listenHue, listenPanelButtons, listenSaveTask, createTaskList, markOccurrences,
+  listenHue, listenPanelButtons, listenSaveTask, listenEditTask, createTaskList, markOccurrences,
   updateProgress, enterEditMode, exitEditMode
 } from "./calendar.js";
 
@@ -131,6 +131,8 @@ listenTaskButtons(taskBtn, closePanel, panel, overlay, calendarWrapper, buttonFo
 listenPanelButtons(addTaskBtn, goBackBtn, taskManager, taskForm, taskList, hueContainer, editTaskBtn, saveTaskBtn);
 listenHue(huePreview, hueContainer, taskHueInput, taskList);
 listenSaveTask(saveTaskBtn, taskNameInput, taskHueInput, huePreview, taskManager, taskForm, tasks, taskList, currentTask, calendarDays, date, progressBar, progressText);
+listenEditTask(editTaskBtn, taskNameInput, taskHueInput, huePreview, taskManager, taskForm, tasks, taskList, currentTask, calendarDays, date, progressBar, progressText);
+
 
 
 
