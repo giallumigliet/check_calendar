cb832019fc35657c99d511a98bc58e4b7d08de2d
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-03-14 14:03:12 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -243,6 +243,7 @@ export function createTaskList(taskList, tasks, currentTask, calendarDays, calen
 
 
   const allTasksBtn = document.createElement("neutral-button");
+  allTasksBtn.style.margin = "0";
   allTasksBtn.textContent = "ALL TASKS";
   taskList.appendChild(allTasksBtn);
 
