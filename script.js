bc67d92a80979325f5bcc51fd761f53421c2c27e
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-03-30 15:21:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -183,22 +183,26 @@ document.addEventListener("click", e => { if (!accountPanel.contains(e.target) &
 // ---- AUTH STATE ----
 onAuthStateChanged(auth, async user => {
   if(user){
-    console.log("USER LOGGED: ", user.uid);
+    console.log("USER logged: ", user.uid);
     calendarWrapper.classList.remove("hidden-task-buttons");
     loginBtn.classList.add("hidden-task-buttons");
     profileBtn.classList.remove("hidden-task-buttons");
     taskBtn.classList.remove("semi-transparent");
     statsBtn.classList.remove("semi-transparent");
     userPhoto.src = user.photoURL;
+    let first = true;
     
     const tasksRef = collection(db, "users", user.uid, "tasks");
     onSnapshot(tasksRef, snapshot => {
       tasks.length = 0;
       snapshot.forEach(doc => { tasks.push({ id: doc.id, ...doc.data() });  });
       createTaskList(taskList, tasks, currentTask, calendarDays, calendarTitle, date, progressWrapper, progressBar, progressText, calendarWrapper, buttonFooter, panel, overlay, taskForm, taskManager, hueContainer, huePreview, editTaskBtn, saveTaskBtn, taskHueInput, taskNameInput);
+      
+      if (first) {
+        await createCalendar(date, monthYear, calendarDays, currentTask, progressBar, progressText, tasks);
+        first = false;
+      }
     });
-
-    await createCalendar(date, monthYear, calendarDays, currentTask, progressBar, progressText, tasks);
 
   } else {
     calendarWrapper.classList.add("hidden-task-buttons");
