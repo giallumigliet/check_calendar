4dd28ea3cceaa08489e567665012f8167eddd246
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-03-30 15:37:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -220,10 +220,7 @@ onAuthStateChanged(auth, async user => {
   }
 });
 
-// ---- INIT UI ----
-//(async () => {
- // await createCalendar(date, monthYear, calendarDays, currentTask, progressBar, progressText, tasks);
-//})();
+// ------------------------------------------------
 listenClickCalendar(addBtn, cancelBtn, taskBtn, dayActions, calendarDays, progressBar, progressText, currentTask, date, message, tasks);
 listenMonthCalendar(date, monthYear, calendarDays, prevMonthBtn, nextMonthBtn, progressBar, progressText, currentTask, tasks);
 listenTaskButtons(taskBtn, statsBtn, closePanel, closeStatsPanel, panel, statsPanel, overlay, calendarWrapper, buttonFooter, taskManager, taskForm, taskList, taskNameInput, taskHueInput, huePreview, currentTask, tasks, chartContainer);
